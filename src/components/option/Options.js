72dbe67e609ac0227dcc43c386e18cf7c9044515
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-11-16 11:50:38 +0200

    Contribute Component removed

--- src/components/option/Options.js
+++ src/components/option/Options.js
@@ -1,36 +1,63 @@
 import React,{useState} from 'react';
 import { CgMenuGridR } from 'react-icons/cg';
-// import { GrClose } from 'react-icons/gr';
-import Contribute from './Contribute';
 import { useDetectClickOutside } from 'react-detect-click-outside';
+import { ContributeForm } from './ContributeForm';
 
 const Options = () => {
     const [isSlideOptions, setIsSlideOptions] = useState(false);
-    
+    const [isContributeFormOpened, setIsContributeFormOpened] = useState(false);
+    const [display, setDisplay] = useState('block');
+    // const openContributeForm = () => {
+    //     setIsContributeFormOpened(true)
+    // }
     const openSlideOptions = () => {
         if (isSlideOptions) {
             setIsSlideOptions(false);
         }
         else {
             setIsSlideOptions(true);
+            // setIsContributeFormOpened(true);
         }
     }
     const closeSlideOptions = () => {
         setIsSlideOptions(false);
+    }
+    const cancel = () => {
+        setIsSlideOptions(false);
+        setIsContributeFormOpened(false)
+        setDisplay('block');
     }
     const ref = useDetectClickOutside({ onTriggered: closeSlideOptions });
 
     return (
         <div ref={ref}>
             <div className='absolute z-10 left-4'>
-                <CgMenuGridR className='text-white text-4xl cursor-pointer' onClick={openSlideOptions}/>
+                <CgMenuGridR className='text-white text-4xl cursor-pointer' onClick={openSlideOptions} />
             </div>
-            {isSlideOptions && <div className='absolute z-10 left-0 pb-5 w-max pr-5 pl-5 pt-5 bg-white top-10'>
-            {/* <GrClose className='text-2xl' onClick={closeSlideOptions} /> */}
-                <div>
-                <Contribute />
+            {
+                isSlideOptions &&
+                <div className='absolute z-10 left-0 pb-5 w-max pr-5 pl-5 pt-5 bg-white top-10 sm:mt-2'>
+                    
+                       
+                            <div>
+                                <button type='button' style={{
+                                    display: display
+                                }} className='w-56 ml-2 h-8 hover:bg-gray-300 text-gray-700' onClick={() => {
+                                    setDisplay('none');
+                                    setIsContributeFormOpened(true);
+                                }}>Contribute</button>
+                        
+                            </div>
+                      
+                    { isContributeFormOpened && <div>
+                        <ContributeForm
+                            cancel={cancel}
+                        />
+                    </div>
+                    }
+                    
                 </div>
-            </div>}
+            }
         </div>
     );
 }
