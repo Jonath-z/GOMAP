f27940dd3f82f1d1c809a5c9a916e909d9ea6da9
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-23 13:33:46 +0200

    search reault window removed

--- src/app/components/header/index.js
+++ src/app/components/header/index.js
@@ -6,7 +6,6 @@ const Header = ({ searchResult }) => {
     return (
         <div>
             <div className='flex justify-center absolute top-2 z-10 w-screen left-0 right-0'>
-                {/* <Search getResult={searchResult}/> */}
                 <Options/>
             </div>
         </div>
