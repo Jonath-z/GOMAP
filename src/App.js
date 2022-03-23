a44b294293c60162a255b03839fa2d084305f017
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-23 14:57:14 +0200

    routing removed to handle the git conflict

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ import MapBox from "./app/components/map";
 import Header from "./app/components/header";
 import LocationsProvider from "./app/context/LocationsProvider";
 
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-
 function App() {
   return (
     <>
       <LocationsProvider>
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Header />
-              <MapBox />
-            </>
-          }
-          />
-        </Routes>
+        <Header />
+        <MapBox />
       </LocationsProvider>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import App from './App';
 import ReactDOM from 'react-dom';
 import './index.css';
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
